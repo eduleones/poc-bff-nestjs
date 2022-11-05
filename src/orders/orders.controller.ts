d186fa0fb1746d5f3126c0f9fbc65e289c0eb032
Eduardo Leones <eduleones@gmail.com>
2022-11-05 17:26:38 -0300

    misc: initial setup

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+}
---
+++ src/orders/orders.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+
+@Injectable()
+export class OrdersService {
+  findAll() {
+    return `This action returns all orders`;
+  }
+}
