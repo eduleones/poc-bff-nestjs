bc8fa622e57134c482d218f13702c50ee0439a05
Eduardo Leones <eduleones@gmail.com>
2022-11-06 12:44:22 -0300

    misc: add uuid

--- src/orders/orders.factories.ts
+++ src/orders/orders.factories.ts
@@ -1,12 +1,13 @@
 import { OrderDto } from "./dto/order.dto";
 import { ItemDto } from "./dto/item.dto";
+import {v4 as uuidv4} from 'uuid';
 
 
 function makeItems(quantity = 10): ItemDto[] {
   const items = [];
   for (let i = 0; i < quantity; i++) {
     const obj = new ItemDto();
-    obj.id = i;
+    obj.id = uuidv4();
     obj.name = `Item ${i}`;
     obj.price = Math.random() * 100;
     obj.quantity = Math.floor(Math.random() * 10);
@@ -19,7 +20,7 @@ function makeOrders(quantity = 10): OrderDto[] {
   const orders = [];
   for (let i = 0; i < quantity; i++) {
     const obj = new OrderDto();
-    obj.id = i;
+    obj.id = uuidv4();
     obj.items = makeItems();
     obj.total = 10.0;
     orders.push(obj);
